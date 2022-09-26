a6e34fecacb5acb0c8f98b7c73aa97e8211f4e25
liamaltker <112987542+liamaltker@users.noreply.github.com>
2022-09-26 12:48:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,184 @@
 
 alert("bingleton hornboggler III");
 alert ("qjkdlsqdfskjgqjkdfsgkjqdfsjkgsdjkfgjksdqfjkgsdfjkqgkjdsfkjgskjdfgkqjjkgfkjgjldfqgkfqqqqqqqqqq");
+    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");    <button onclick="myFunction()" ">A mysterious clicky thing</button>
+                                    <script> 
+                                    function myFunction() {
+                                    alert ("haha you clicked");
