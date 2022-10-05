d6f3734b0dcd0d54ce5aa9a3bce8119560601c05
liamaltker <112987542+liamaltker@users.noreply.github.com>
2022-10-05 13:39:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ function myFunction69()
 { alert ("sorry this button is not working please go to the free robux 2.0 button");
 }
 <script>
+    
+ <button onclick="myFunction2000()">Free robux 2.0!!</button>
+
+<script> 
+     function myFunction2000
+ { alert ("01000111 01101111 00100000 01110100 01101111 00100000 01100001 01101100 01110100 01101011 00100000 01100010 01100101 01100011 01100001 01110101 01110011 01100101 00100000 01101001 01110100 01110011 00100000 01100011 01101111 01101111 01101100 https://www.binarytranslator.com");
+ }
+<script>
+      
                                           
