0c0923b8092097f94bd1dbadb77768781633f0b0
liamaltker <112987542+liamaltker@users.noreply.github.com>
2022-10-12 13:14:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 
 alert("bingleton hornboggler III");
 alert ("qjkdlsqdfskjgqjkdfsgkjqdfsjkgsdjkfgjksdqfjkgsdfjkqgkjdsfkjgskjdfgkqjjkgfkjgjldfqgkfqqqqqqqqqq");
-  <script> 
-      function visitPage() 
-  { window.location.redirect='https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwic4szHhtv6AhUwFjQIHaWJCmQQu6AFegQIDRAB&url=https%3A%2F%2Fwww.tiktok.com%2F%40vexbolts%2Fvideo%2F7152680771822570795&usg=AOvVaw2xEzfsEUuAOE3AMKw';
-  } 
-</script>
-      button onclick="visitPage();">The ultimate button</button>
-    <script> 
-        { function visitPage() window.location.redirect='https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwic4szHhtv6AhUwFjQIHaWJCmQQu6AFegQIDRAB&url=https%3A%2F%2Fwww.tiktok.com%2F%40vexbolts%2Fvideo%2F7152680771822570795&usg=AOvVaw2xEzfsEUuAOE3AMKw';
-  } 
-</script>
-
+button onclick="functionRedirect"
