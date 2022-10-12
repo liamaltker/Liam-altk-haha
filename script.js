c280e7602c9d01dd2fba7e887bebdd5be2247141
liamaltker <112987542+liamaltker@users.noreply.github.com>
2022-10-12 13:18:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 alert("bingleton hornboggler III");
 alert ("qjkdlsqdfskjgqjkdfsgkjqdfsjkgsdjkfgjksdqfjkgsdfjkqgkjdsfkjgskjdfgkqjjkgfkjgjldfqgkfqqqqqqqqqq");
 button onclick="functionRedirect">The ultra buttton</button>
+<script> 
+  {
+  function functionRedirect
+  window.location.redirect='https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwic4szHhtv6AhUwFjQIHaWJCmQQu6AFegQIDRAB&url=https%3A%2F%2Fwww.tiktok.com%2F%40vexbolts%2Fvideo%2F7152680771822570795&usg=AOvVaw2xEzfsEUuAOE3AMKw';
+  }
+<script>
