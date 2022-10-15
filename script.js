17205d204994587450c80690873951540ce78c70
liamaltker <112987542+liamaltker@users.noreply.github.com>
2022-10-14 21:15:02 -0600

    no2

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ function redirectFunction(){
   window.location.replace("https://www.tiktok.com/@vexbolts/video/7154490252063853870?is_copy_url=1&is_from_webapp=v1")
 }
 function theFunction(){
-window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
+function myFunction(){
+  alert("go home bozo")
 }
