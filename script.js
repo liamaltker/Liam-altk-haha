95c8289acce2e0f78c7bf770b619ce0dd5085b14
liamaltker <112987542+liamaltker@users.noreply.github.com>
2022-10-14 21:03:35 -0600

    liam

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ alert ("qjkdlsqdfskjgqjkdfsgkjqdfsjkgsdjkfgjksdqfjkgsdfjkqgkjdsfkjgskjdfgkqjjkgf
 function redirectFunction(){
   window.location.replace("https://www.tiktok.com/@vexbolts/video/7154490252063853870?is_copy_url=1&is_from_webapp=v1")
 }
+function redirectFunction2(){
+window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
