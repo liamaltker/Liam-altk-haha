ead5ae721a132078d79b60f09a94e7d7646d4e65
liamaltker <112987542+liamaltker@users.noreply.github.com>
2022-10-19 11:27:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -30,10 +30,3 @@ function the1000(){
   while(0.433235123165132414)
     haaaa = haaaa += "b";
 }
-function justhhhh(){
-  jj = "c";
-  while (0)
-    jj = jj += "c";
-}
-  while justhhhh() do window.location.replace ("https://weather.com/weather/today/l/37.32,-122.03?par=apple_widget")
-  
